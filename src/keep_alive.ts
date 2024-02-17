0665defc29724c2cce26342ccd8dfac105849266
Tarit Mahato <122891749+mahatotarit@users.noreply.github.com>
2024-02-17 18:15:50 +0530

    Update keep_alive.ts

--- src/keep_alive.ts
+++ src/keep_alive.ts
@@ -1,6 +1,8 @@
 import "http";
 
-export http.createServer(function (req, res) {
+const port_o = http.createServer(function (req, res) {
   res.write("I'm alive");
   res.end();
 }).listen(8080);
+
+export default port_o;
