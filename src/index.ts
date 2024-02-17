94fe989db676e0f74d482efecec5dfd655928616
Tarit Mahato <122891749+mahatotarit@users.noreply.github.com>
2024-02-17 18:30:51 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import "log-timestamp";
 import { providers, Wallet } from "ethers";
 
+import http from 'http';
+
+// Create a server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!\n');
+});
+
+// Listen on port 8080
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 // If you're using an Infura RPC URL, please uncomment the following two lines:
 import { EventEmitter } from 'events';
 EventEmitter.defaultMaxListeners = 1000;
